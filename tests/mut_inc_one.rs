16f9bb0e82862003f99f0b383bf600684d5ccfe4
Linus Färnstrand <faern@faern.net>
2015-04-16 11:51:27 +0200

    Fixing segfault in mut_inc_one

--- tests/mut_inc_one.rs
+++ tests/mut_inc_one.rs
@@ -13,29 +13,26 @@ fn mut_inc_one_to_five() {
 
     let forkpool = ForkPool::with_threads(4);
 
-    let result_port = forkpool.schedule(mut_inc_task, data);
+    let result_port = forkpool.schedule(mut_inc_task, &mut data[..]);
     result_port.recv().unwrap();
 
     assert_eq!(vec![2,3,4,5,6], unsafe_data);
+    unsafe { mem::forget(unsafe_data); }
 }
 
 #[cfg(test)]
-fn mut_inc_task(d: Vec<usize>) -> TaskResult<Vec<usize>, ()> {
-    let mut data = d;
-    let len = data.len();
+fn mut_inc_task(d: &mut [usize]) -> TaskResult<&mut [usize], ()> {
+    let len = d.len();
     if len == 1 {
-        data[0] += 1;
+        d[0] += 1;
         TaskResult::Done(())
     } else {
-        let l1 = len/2;
-        let l2 = len-l1;
-        let p = data.as_mut_ptr();
-        unsafe {
-            mem::forget(data);
-            let v1: Vec<usize> = Vec::from_raw_parts(p, l1, l1);
-            let v2: Vec<usize> = Vec::from_raw_parts(p.offset(l1 as isize), l2, l2);
-            TaskResult::Fork(Fork{fun: mut_inc_task, args: vec![v1,v2], join: AlgoStyle::Summa(mut_inc_join)})
-        }
+        let (d1,d2) = d.split_at_mut(len/2);
+        TaskResult::Fork(Fork {
+            fun: mut_inc_task,
+            args: vec![d1, d2],
+            join: AlgoStyle::Summa(mut_inc_join)
+        })
     }
 }
 
